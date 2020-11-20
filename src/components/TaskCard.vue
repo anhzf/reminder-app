9c482659dda6d7c78dd4799c2df23a0ffc532e8a
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-20 09:09:51 +0700

    feat: :lipstick: MyTask list UI

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-list
+    padding
+    class="task-card"
+  >
+    <q-item
+      v-for="(item, i) in tasks"
+      :key="i"
+      clickable
+      v-ripple
+      :class="{'bg-blue-grey-1': item.done, 'text-grey-7': item.done}"
+      @click="item.done = !item.done"
+    >
+      <q-item-section>
+        <q-item-label>{{ item.todo }}</q-item-label>
+      </q-item-section>
+      <q-item-section side>
+        <q-checkbox
+          v-model="item.done"
+          color="blue"
+        />
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'TaskCard',
+
+  setup() {
+    const tasks = ref([...Array(~~(Math.random() * 100))].map((v, i) => ({ todo: `task ${i}`, done: false })));
+    return { tasks };
+  },
+});
+</script>
+
+<style lang="scss">
+.task-card {
+  width: 100%;
+  max-width: $breakpoint-sm;
+  padding: map-get($space-md, y);
+}
+</style>
--- src/pages/MyTask.vue
+++ src/pages/MyTask.vue
@@ -1,29 +1,36 @@
 <template>
-  <q-card
-    bordered
-    flat
-  >
-    <q-list>
-      <q-item>
-        Lorem ipsum dolor sit amet.
-      </q-item>
-      <q-item>
-        Lorem ipsum dolor sit amet.
-      </q-item>
-      <q-item>
-        Lorem ipsum dolor sit amet.
-      </q-item>
-      <q-item>
-        Lorem ipsum dolor sit amet.
-      </q-item>
-    </q-list>
-  </q-card>
+  <q-page class="q-py-md column items-center">
+    <h2 class="text-h4">
+      My Daily Task
+    </h2>
+
+    <task-card />
+
+    <q-page-sticky
+      position="bottom-right"
+      :offset="[35, 35]"
+    >
+      <q-btn
+        fab
+        icon="edit"
+        round
+        size="lg"
+        color="light-blue-6"
+        text-color="white"
+      />
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import TaskCard from 'components/TaskCard.vue';
 
 export default defineComponent({
   name: 'PageMyTask',
+
+  components: {
+    TaskCard,
+  },
 });
 </script>
